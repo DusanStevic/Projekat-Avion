8b5e178d0377dda3372eaefd7183d9dd4e5c102c
Dusan Stevic <stevicdule@gmail.com>
2019-09-11 10:38:59 +0200

    controller package

--- Avion/Avion/src/main/resources/data.sql
+++ Avion/Avion/src/main/resources/data.sql
@@ -2,11 +2,11 @@
 -- Lozinka za oba user-a je 123
 -- Ovo su sve imena kolona u tabelama db(Column anotacija u klasama), a ne imena polja u klasama
 
-INSERT INTO Authorities (id, role) VALUES (1, "ROLE_SYSTEM_ADMIN");
-INSERT INTO Authorities (id, role) VALUES (2, "ROLE_AIRLINE_ADMIN");
-INSERT INTO Authorities (id, role) VALUES (3, "ROLE_HOTEL_ADMIN");
-INSERT INTO Authorities (id, role) VALUES (4, "ROLE_RENT_A_CAR_ADMIN");
-INSERT INTO Authorities (id, role) VALUES (5, "ROLE_REGISTERED_USER");
+INSERT INTO Authorities (id, role) VALUES (1, 'ROLE_SYSTEM_ADMIN');
+INSERT INTO Authorities (id, role) VALUES (2, 'ROLE_AIRLINE_ADMIN');
+INSERT INTO Authorities (id, role) VALUES (3, 'ROLE_HOTEL_ADMIN');
+INSERT INTO Authorities (id, role) VALUES (4, 'ROLE_RENT_A_CAR_ADMIN');
+INSERT INTO Authorities (id, role) VALUES (5, 'ROLE_REGISTERED_USER');
         
         
         
