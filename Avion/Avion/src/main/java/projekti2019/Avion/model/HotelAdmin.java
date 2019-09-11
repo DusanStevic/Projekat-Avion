42f2fbaf8fd1ea013c036ff67093f2d999417286
Dusan Stevic <stevicdule@gmail.com>
2019-09-11 10:01:15 +0200

    users

---
+++ Avion/Avion/src/main/java/projekti2019/Avion/model/HotelAdmin.java
@@ -0,0 +1,13 @@
+package projekti2019.Avion.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class HotelAdmin extends User {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ Avion/Avion/src/main/java/projekti2019/Avion/model/RegisteredUser.java
@@ -0,0 +1,13 @@
+package projekti2019.Avion.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class RegisteredUser extends User {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
